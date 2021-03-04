2f4c3b724ef1a895819920856851f345182e313b
Geonho Kim <gunhoflash@gmail.com>
2021-03-04 14:44:00 +0900

    Redesign

--- frontend/my-app/src/pages/Home.jsx
+++ frontend/my-app/src/pages/Home.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
-
 import Calendar from '../components/Calendar';
-
-import styled from 'styled-components';
-
-import ATag from '../components/Button';
-
-import { Link } from 'react-router-dom';
-
-const HomeContainer = styled.div`
-  display : flex;
-  flex-direction : column;
-  align-items : center;
-`;
-
-const LinkContainer = styled.div`
-  padding-bottom : 10px;
-`;
 
 export default function Home() {
   return (
-    <HomeContainer>
-      <LinkContainer>
-        <Link to="/newDiary"><ATag>일기 작성하기</ATag></Link>
-      </LinkContainer>
+    <div className="container vh-100 d-flex flex-column align-items-center justify-content-center">
+      <a href="/newDiary" className="btn btn-primary mb-3" role="button">
+        일기 작성하기
+      </a>
       <Calendar />
-    </HomeContainer>
+    </div>
   )
   // happy, soso , bad, melancholy,
   // 노란색, 회색, 빨간색, 파란색 #5442f5
